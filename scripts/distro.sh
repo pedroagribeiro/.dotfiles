974a4868989897a16818543a2378a4c7c984ad77
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2024-09-02 17:54:11 +0100

    [asdf & git] => Adds asdf and git configurations

---
+++ scripts/distro.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function log_section() {
+  local LABEL="$1"
+  local COLOR="$PURPLE"
+  printf "[${COLOR}${BOLD}${LABEL}${RESET}]%*s\n" $(($(tput cols) - ${#LABEL} - 2)) | sed -e 's/ /═/g'
+}
+
+function symlink() {
+  local SRC="$1"
+  local DST="$2"
+
+  execute "ln -sfT ${SRC} ${DST}" "Symlinking $(basename $DST)"
+}
+
+function install_package() {
+  local PKG="$1"
+
+  execute "yay -Syyu ${PKG} --needed --noconfirm" "Installing ${BOLD}${PKG}${RESET}..."
+}
