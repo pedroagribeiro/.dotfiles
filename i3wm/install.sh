bfc65ec6306d2591b2ba25006a29ae486406631e
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2024-09-04 09:53:57 +0100

    [i3wm]: Updates installation script to reference the correct path

--- i3wm/install.sh
+++ i3wm/install.sh
@@ -3,8 +3,10 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
-# shellcheck source=../scripts/extras.sh
-. scripts/extras.sh
+# shellcheck source=../scripts/execs.sh
+. scripts/execs.sh
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/utils.sh
 . scripts/utils.sh
 
@@ -13,4 +15,4 @@ ask_for_sudo
 install_package i3
 install_package j4-dmenu-desktop
 
-symlink "$HOME/.dotfiles/i3/config" "$HOME/.config/i3/config"
+symlink "$HOME/.dotfiles/i3wm/config" "$HOME/.config/i3/config"
