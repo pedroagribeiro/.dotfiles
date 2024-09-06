03b1d145ca7f74316cb9b4333695b260c4cf91b1
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2024-09-06 11:23:01 +0100

    [1Password]: Adds 1Password agent configuration

---
+++ 1Password/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/execs.sh
+. scripts/execs.sh
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+symlink_dir ~/.dotfiles/1Password/ssh ~/.config/1Password/ssh
---
+++ 1Password/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+unlink ~/.config/1Password/ssh
