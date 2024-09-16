c425d6ed623077f0a9707356c623d97bf3e24919
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2024-09-16 18:57:17 +0100

    [bspwm]: Adds bspwm window manager configuration

---
+++ bspwm/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/execs.sh
+. scripts/execs.sh
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+ask_for_sudo
+
+# install_package bspwm 
+
+symlink "$HOME/.dotfiles/bspwm/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
