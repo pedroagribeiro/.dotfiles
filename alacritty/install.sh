5fe2e785045443435d232fac38f3bb083e1d0c79
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2024-09-17 14:52:11 +0100

    [alacritty]: Refactors theme storing structure

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -3,13 +3,15 @@
 BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 cd "${BASE_DIR}/.." || exit 127
 
-# shellcheck source=../scripts/extras.sh
-. scripts/extras.sh
+# shellcheck source=../scripts/execs.sh
+. scripts/execs.sh
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
 # shellcheck source=../scripts/utils.sh
 . scripts/utils.sh
 
 # ask_for_sudo
 
-install_package alacritty
+# install_package alacritty
 
 symlink "$HOME/.dotfiles/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
