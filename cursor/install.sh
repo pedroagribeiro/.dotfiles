eec6a7d451bc04da47f2d3afc8ff25ef640e7d71
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2025-04-09 21:02:12 +0100

    [cursor]: Adds cursor configuration folder

---
+++ cursor/install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/execs.sh
+. scripts/execs.sh
+# shellcheck source=../scripts/distro.sh
+. scripts/distro.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+
+LINUX_CONF_DIR="$HOME/.config/Cursor/User"
+MACOS_CONF_DIR="$HOME/Library/Application\ Support/Cursor/User"
+
+EXTENSIONS=()
+
+while IFS= read -r line; do
+    EXTENSIONS+=("$line")
+done < "$HOME/.dotfiles/vscode/extensions.txt"
+
+# for extension in "${EXTENSIONS[@]}"; do
+#     cursor --install-extension $extension
+# done
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    mkdir -p $MACOS_CONF_DIR
+    symlink "$HOME/.dotfiles/vscode/settings.json" "$MACOS_CONF_DIR/settings.json"
+else
+    mkdir -p $LINUX_CONF_DIR
+    symlink "$HOME/.dotfiles/vscode/settings.json" "$LINUX_CONF_DIR/settings.json"
+    cp "$HOME/.dotfiles/vscode/settings.json" "/mnt/c/Users/Pedro\ Ribeiro/AppData/Roaming/Cursor/User/settings.json"
+fi
