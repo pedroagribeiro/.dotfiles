38363d169be24b6e722fa5a843de5f0d09f7a3a7
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2025-04-10 01:48:34 +0100

    [scripts]: Updates scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:nelsonmestevao/dotfiles.git ~/.dotfiles
-git clone git@github.com:nelsonmestevao/spells.git ~/.spells
+git clone git@github.com:pedroagribeiro/dotfiles.git ~/.dotfiles
+git clone git@github.com:pedroagribeiro/spells.git ~/.spells
 
 cd ~/.dotfiles
 ./install
