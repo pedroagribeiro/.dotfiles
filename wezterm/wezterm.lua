783016b954986741dcac59df8f67a98b7cf95581
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2025-11-17 10:33:34 +0000

    [wezterm]: Comments shortcuts in order to solve conflict

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,8 +16,8 @@ config.window_background_opacity = 0.9
 config.macos_window_background_blur = 30
 config.win32_system_backdrop = "Acrylic"
 
-config.color_scheme = "Dark+"
-config.font = wezterm.font('GeistMono Nerd Font')
+config.color_scheme = "GitHub Dark"
+config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 15
 
 config.enable_scroll_bar = false
@@ -28,59 +28,59 @@ config.show_tabs_in_tab_bar = true
 config.show_new_tab_button_in_tab_bar = false
 config.tab_bar_at_bottom = true
 
-config.leader = {
-  -- https://github.com/wez/wezterm/issues/4055#issuecomment-2137129562
-  key = ' ',
-  mods = 'CTRL',
-  timeout_milliseconds = 1000
-}
-
-config.keys = {
-  {
-    key = 'v',
-    mods = 'LEADER',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'h',
-    mods = 'LEADER',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'p',
-    mods = 'LEADER',
-    action = wezterm.action.PaneSelect {
-      mode = 'Activate',
-    },
-  },
-  {
-    key = 'l',
-    mods = 'META',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'h',
-    mods = 'META',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'k',
-    mods = 'META',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'j',
-    mods = 'META',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
-}
-
-for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'META',
-    action = wezterm.action.ActivateTab(i - 1),
-  })
-end
-
+-- config.leader = {
+--   -- https://github.com/wez/wezterm/issues/4055#issuecomment-2137129562
+--   key = ' ',
+--   mods = 'CTRL',
+--   timeout_milliseconds = 1000
+-- }
+--
+-- config.keys = {
+--   {
+--     key = 'v',
+--     mods = 'LEADER',
+--     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+--   },
+--   {
+--     key = 'h',
+--     mods = 'LEADER',
+--     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+--   },
+--   {
+--     key = 'p',
+--     mods = 'LEADER',
+--     action = wezterm.action.PaneSelect {
+--       mode = 'Activate',
+--     },
+--   },
+--   {
+--     key = 'l',
+--     mods = 'META',
+--     action = wezterm.action.ActivatePaneDirection 'Right',
+--   },
+--   {
+--     key = 'h',
+--     mods = 'META',
+--     action = wezterm.action.ActivatePaneDirection 'Left',
+--   },
+--   {
+--     key = 'k',
+--     mods = 'META',
+--     action = wezterm.action.ActivatePaneDirection 'Up',
+--   },
+--   {
+--     key = 'j',
+--     mods = 'META',
+--     action = wezterm.action.ActivatePaneDirection 'Down',
+--   },
+-- }
+--
+-- for i = 1, 9 do
+--   table.insert(config.keys, {
+--     key = tostring(i),
+--     mods = 'META',
+--     action = wezterm.action.ActivateTab(i - 1),
+--   })
+-- end
+--
 return config
